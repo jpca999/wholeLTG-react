e78a433855cc3dee788eb4ee2c4877f2a465cc52
jpca999 <jpca999@gmail.com>
2019-12-05 22:29:01 -0800

    faq page working now

--- src/pages/Faq/Index.js
+++ src/pages/Faq/Index.js
@@ -1,17 +1,20 @@
 import React from 'react'; 
 // import { Col, Row, Container, Form } from 'react-bootstrap';
-// import './Footer.scss'; 
 
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import './Faq.scss'; 
+// import FacebookIcon from '@material-ui/icons/Facebook';
 
-// import mylogo from './mylogo.png'; 
-import mylogo from './mylogo.png'; 
+import mylogo from '../../Images/mylogo.png';
 
 const useStyles = makeStyles(theme => ({
     root: {
       flexGrow: 1,
+      background: '#f04134',
+      paddingTop: '45px',
+      height: '500px',
     },
     paper: {
       padding: theme.spacing(2),
@@ -28,16 +31,55 @@ const useStyles = makeStyles(theme => ({
         <Grid container spacing={1}>
         {/* xs=3 LTG icon */}
           <Grid item xs={4}>
-            <div   className="footLogo">
-                <img className="footLogo" alt="foot logo" src={mylogo} />
+            <div className="text-center">
+                <img className="" alt="foot logo" src={mylogo} />
+                <p className="text-center footer-logo-p">
+                Copyright 2019 - Liberty Trust Group, Inc.
+                All Rights Reserved. *Each franchise office is
+                independently owned and operated.
+                </p>
                 </div>
           </Grid>
-          <Grid item xs={4}>
-            <Paper className={classes.paper}> first + second li</Paper>
+          <Grid item xs={2}>
+            <div className={classes.paper}> 
+            <ul className="ulalign">
+            <li className="white-footer-link"> <a className="white-footer-link" href="http://">How it Works</a> </li>
+            <li className="white-footer-link">  <a className="white-footer-link" href="http://">FAQ</a></li>
+            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Locations</a></li>
+            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Reviews</a></li>
+            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Contact</a></li>
+            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Sell</a></li>
+            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Sell</a></li>
+          </ul>
+            </div>
           </Grid>
-          <Grid item xs={4}>
-            <Paper className={classes.paper}> Fb icons </Paper>
+          <Grid item xs={3}>
+            <div className={classes.paper}> 
+            <ul className="ulalign">
+            <li className="white-footer-link"> <a className="white-footer-link" href="http://">We Pay Cash</a> </li>
+            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Blog</a></li>
+            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Company Info</a></li>
+            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Franchise Info</a></li>
+            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Liberty Trust Group</a></li>
+            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Privacy</a></li>
+            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Terms of Service</a></li>
+            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Site Map</a></li>
+          </ul>      
+             </div>
           </Grid>
+
+
+          <Grid item xs={2}>
+            <div className={classes.paper}> 
+
+                Facebook icon
+             </div>
+          </Grid>
+
+
+
+
+
         </Grid>
       </div>
     );
