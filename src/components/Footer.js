07e9497038e15ec3474ae233656fb03baec832fd
jpca999 <jpca999@gmail.com>
2019-12-05 22:32:49 -0800

    Faq is now put into Footer is working

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,36 +1,58 @@
 import React from 'react'; 
-import { Col, Row, Container, Form } from 'react-bootstrap';
+// import { Col, Row, Container, Form } from 'react-bootstrap';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import './Footer.scss'; 
 
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-// import Container from '@material-ui/core/Container';
+import mylogo from '../Images/mylogo.png';
 
-
-
-const Footer = () =>{
-return  <div> 
-<Container fluid={true} className="footer">
-  <div className="col-sm-4 col-xs-12 footer-logo">
-    <img alt="liberty buys housess" src="mylogo.png" />
-    <p className="p-belowLogo">Copyright 2019 - Liberty Trust Group, Inc. </p>
-    <p className="p-belowLogo">      All Rights Reserved. *Each franchise office is </p>
-    <p className="p-belowLogo">  independently owned and operated.</p>
-    <div className="col-sm-4 col-xs-12">
-      <Row>
-        <Col className="col-sm-6 col-xs-12">
-          <ul>
+const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+      background: '#f04134',
+      paddingTop: '45px',
+      height: '500px',
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }));
+  
+ function Footer() {
+    const classes = useStyles();
+  
+    return (
+      <div className={classes.root}>
+        <Grid container spacing={1}>
+        {/* xs=3 LTG icon */}
+          <Grid item xs={4}>
+            <div className="text-center">
+                <img className="" alt="foot logo" src={mylogo} />
+                <p className="text-center footer-logo-p">
+                Copyright 2019 - Liberty Trust Group, Inc.
+                All Rights Reserved. *Each franchise office is
+                independently owned and operated.
+                </p>
+                </div>
+          </Grid>
+          <Grid item xs={2}>
+            <div className={classes.paper}> 
+            <ul className="ulalign">
             <li className="white-footer-link"> <a className="white-footer-link" href="http://">How it Works</a> </li>
-            <li className="white-footer-link">  <a className="white-footer-link" href="http://">FAQ</a></li>
+            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Footer</a></li>
             <li className="white-footer-link">  <a className="white-footer-link" href="http://">Locations</a></li>
             <li className="white-footer-link">  <a className="white-footer-link" href="http://">Reviews</a></li>
             <li className="white-footer-link">  <a className="white-footer-link" href="http://">Contact</a></li>
             <li className="white-footer-link">  <a className="white-footer-link" href="http://">Sell</a></li>
             <li className="white-footer-link">  <a className="white-footer-link" href="http://">Sell</a></li>
           </ul>
-        </Col>
-        <div className="col-sm-6 col-xs-12">
-          <ul>
+            </div>
+          </Grid>
+          <Grid item xs={3}>
+            <div className={classes.paper}> 
+            <ul className="ulalign">
             <li className="white-footer-link"> <a className="white-footer-link" href="http://">We Pay Cash</a> </li>
             <li className="white-footer-link">  <a className="white-footer-link" href="http://">Blog</a></li>
             <li className="white-footer-link">  <a className="white-footer-link" href="http://">Company Info</a></li>
@@ -39,20 +61,35 @@ return  <div>
             <li className="white-footer-link">  <a className="white-footer-link" href="http://">Privacy</a></li>
             <li className="white-footer-link">  <a className="white-footer-link" href="http://">Terms of Service</a></li>
             <li className="white-footer-link">  <a className="white-footer-link" href="http://">Site Map</a></li>
-          </ul>
-        </div>
-      </Row>                                
-    </div>
-    <Col className="col-sm-4 col-xs-12">
-      <ul>
-        <li className="white-footer-link"> <a className="white-footer-link" href="http://">fb icon</a> </li>
-        <li className="white-footer-link">  <a className="white-footer-link" href="http://">Twitter icon</a></li>
-      </ul>
-    </Col>                                            
-  </div>
-  </Container>
-  </div>
+          </ul>      
+             </div>
+          </Grid>
 
-}
+
+          <Grid item xs={2}>
+            <div className={classes.paper}> 
+
+                Facebook icon
+             </div>
+          </Grid>
+
+
+
+
+
+        </Grid>
+      </div>
+    );
+  }
+
+  
+
+
+// const Footer = () =>{
+//         return <div> 
+            
+//     </div>
+
+// }
 
 export default Footer; 
