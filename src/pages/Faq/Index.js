6fb82f2d6b8b283226a51ebf759755badf79522b
jpca999 <jpca999@gmail.com>
2019-12-07 18:31:23 -0800

    -

--- src/pages/Faq/Index.js
+++ src/pages/Faq/Index.js
@@ -1,98 +1,72 @@
-import React from 'react'; 
-// import { Col, Row, Container, Form } from 'react-bootstrap';
-
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import './Faq.scss'; 
-// import FacebookIcon from '@material-ui/icons/Facebook';
+import Typography from '@material-ui/core/Typography';
 
-import mylogo from '../../Images/mylogo.png';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1,
-      background: '#f04134',
-      paddingTop: '45px',
-      height: '500px',
-    },
-    paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
-  }));
-  
- function Faq() {
-    const classes = useStyles();
-  
-    return (
-      <div className={classes.root}>
-        <Grid container spacing={1}>
-        {/* xs=3 LTG icon */}
-          <Grid item xs={4}>
-            <div className="text-center">
-                <img className="" alt="foot logo" src={mylogo} />
-                <p className="text-center footer-logo-p">
-                Copyright 2019 - Liberty Trust Group, Inc.
-                All Rights Reserved. *Each franchise office is
-                independently owned and operated.
-                </p>
-                </div>
-          </Grid>
-          <Grid item xs={2}>
-            <div className={classes.paper}> 
-            <ul className="ulalign">
-            <li className="white-footer-link"> <a className="white-footer-link" href="http://">How it Works</a> </li>
-            <li className="white-footer-link">  <a className="white-footer-link" href="http://">FAQ</a></li>
-            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Locations</a></li>
-            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Reviews</a></li>
-            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Contact</a></li>
-            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Sell</a></li>
-            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Sell</a></li>
-          </ul>
-            </div>
-          </Grid>
-          <Grid item xs={3}>
-            <div className={classes.paper}> 
-            <ul className="ulalign">
-            <li className="white-footer-link"> <a className="white-footer-link" href="http://">We Pay Cash</a> </li>
-            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Blog</a></li>
-            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Company Info</a></li>
-            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Franchise Info</a></li>
-            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Liberty Trust Group</a></li>
-            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Privacy</a></li>
-            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Terms of Service</a></li>
-            <li className="white-footer-link">  <a className="white-footer-link" href="http://">Site Map</a></li>
-          </ul>      
-             </div>
-          </Grid>
+  root: {
+    padding: theme.spacing(3, 2),
+    height: 300,
+  },
+}));
 
+export default function Faq() {
+  const classes = useStyles();
 
-          <Grid item xs={2}>
-            <div className={classes.paper}> 
-
-                Facebook icon
-             </div>
-          </Grid>
-
-
-
-
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={1}>
+      {/* xs=3 LTG icon */}
+        
+        <Grid item xs={4}>
+          <Paper className={classes.root}>
+              <Typography variant="h5" component="h3">
+                This is a sheet of paper.
+              </Typography>
+              <Typography variant="h5" component="h3">
+                First -  People that work at LibertyTrustGroup made the experience very pleasant. 
+              </Typography>
+          </Paper>
 
         </Grid>
-      </div>
-    );
-  }
-
-  
 
 
-// const Faq = () =>{
-//         return <div> 
-            
-//     </div>
+        <Grid item xs={4}>
 
-// }
+<Paper className={classes.root}>
+    <Typography variant="h5" component="h3">
+      This is a sheet of paper.
+    </Typography>
+    <Typography component="p">
+      Paper can be used to build surface or other elements for your application.
+    </Typography>
+</Paper>
 
-export default Faq; 
+</Grid>
+
+
+
+<Grid item xs={4}>
+
+          <Paper className={classes.root}>
+              <Typography variant="h5" component="h3">
+                This is a sheet of paper.
+              </Typography>
+              <Typography component="p">
+                Paper can be used to build surface or other elements for your application.
+              </Typography>
+          </Paper>
+
+        </Grid>
+
+
+      </Grid>
+    </div>
+  );
+ 
+}
+
+
+
--- todo.md
+++ todo.md
@@ -1,5 +1,12 @@
-create the icon for the header top left. 
 
+
+
+
+================================================
+Replace testimonials on index.js with paper. 
+
+======================
+create the icon for the header top left. 
 
 https://magic.reactjs.net/htmltojsx.htm
 
